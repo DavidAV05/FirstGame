e3800aa57180027842ec017846e13cabce0ad294
David Angulo Velez <David Angulo Velez>
2024-02-05 15:30:28 +0100

    Basic controls finished

---
+++ game/player/player.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -400.0
+
+var dash = true
+@onready var dash_timer = $dash_timer
+const dash_cooldown = .5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var TERMINAL_VELOCITY = gravity * 3
+
+# Handles all the basic horizontal movement
+func move_horizontal (direction):
+	# Checks if player is walking
+	velocity.x = move_toward(velocity.x, SPEED * direction, SPEED / 20)
+
+
+# Handles all the dash abilities
+func dash_func(direction):
+	# Checks if dashing is not activated
+	if dash_timer.is_stopped():
+		# Starts timer
+		dash_timer.wait_time = dash_cooldown
+		dash_timer.start()
+		# Define dash up
+		if Input.is_action_pressed("move_up"):
+			velocity.y += JUMP_VELOCITY * 1.5
+		# Define dash down
+		elif Input.is_action_pressed("move_down"):
+			velocity.y += -JUMP_VELOCITY * 1.5
+		# Define dash right and left
+		else:
+			velocity.x += SPEED * 2 * direction
+
+	dash = false
+
+
+
+func _physics_process(delta):
+	print(dash_timer.is_stopped())
+	move_and_slide()
+
+	# Add the gravity and dash "recharge".
+	if !is_on_floor() and velocity.y <= TERMINAL_VELOCITY:
+		velocity.y += gravity * delta
+	else:
+		dash = true
+
+	# Gets direction
+	var direction = Input.get_axis("move_left", "move_right")
+
+	# Handle the horizontal movement/deceleration.
+	move_horizontal(direction)
+		
+	# Handles dashing
+	if Input.is_action_just_pressed("dash") and dash == true:
+		dash_func(direction)
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
