91cae0a45e161864074b22ae8458ca51522e090a
David Angulo Velez <David Angulo Velez>
2024-02-20 21:07:37 +0100

    Revamped wall climbing, dash broken

--- game/Characters/player/player.gd
+++ game/Characters/player/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
-@export var SPEED = 200.0
-@export var JUMP_VELOCITY = -500.0
-@export var WALL_CLIMB_SPEED = 300
+@export var SPEED := 200.0
+@export var JUMP_VELOCITY := -500.0
+@export var WALL_CLIMB_SPEED := 200.0
 
-# Dash variables
-@onready var dash_timer = $DashTimer
-@export var DASH_COOLDOWN = .5
+# Global variables
+var dash: bool = true
+var climb_dir: int = 1
 
 # Coyote variables
-@onready var Coyote_Jump = $CoyoteJump
+@onready var Coyote_Jump = %CoyoteJump
+
+# Dash variables
+@onready var dash_timer = %DashTimer
+@export var DASH_COOLDOWN = .5
 
 # Gets all the camera variables
-@onready var camera = $Camera2D
+@onready var camera = %Camera2D
 @onready var camera_lag = camera.position_smoothing_enabled
 @onready var camera_lag_speed = camera.position_smoothing_speed
 
@@ -21,15 +25,21 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var TERMINAL_VELOCITY = gravity * 3
 
 func gravity_func(delta: float, on_floor: bool, on_wall: bool) -> void:
+	# When going upwards gravity:
 	if !on_floor and !on_wall and velocity.y <= 0:
 		velocity.y += gravity * delta
+	# When going downwards gravity:
 	elif !on_floor and !on_wall and velocity.y < TERMINAL_VELOCITY:
-		velocity.y += gravity * delta * 1.25
+		velocity.y += gravity * delta * 1.35
+
+
 
 # Handles all the basic horizontal movement
 func move_horizontal(direction: int) -> void:
 	# Checks if player is walking
 	velocity.x = move_toward(velocity.x, SPEED * direction, SPEED / 10)
+
+
 
 # Handles jump input, check next to gravity for coyote timer
 func jump_func(can_jump: bool) -> void:
@@ -37,28 +47,30 @@ func jump_func(can_jump: bool) -> void:
 		can_jump = false
 		velocity.y = JUMP_VELOCITY
 
-# Handles the wall sliding of character.
-func wall_slide_func(delta: float, on_floor: bool, on_wall: bool) -> void:
-	var direction := 0
-	var climbing := 1
-	if velocity.y >= 0:
-		direction = 1
-	else:
-		direction = -1	
-	
-	if  Input.is_action_pressed("jump"):
-		direction = 0
-		climbing = 1.5
 
-	if on_wall:
-		print(WALL_CLIMB_SPEED * direction)
-		print(WALL_CLIMB_SPEED * delta * .5)
-		velocity.y = move_toward(velocity.y,
-			WALL_CLIMB_SPEED * direction, 
-			WALL_CLIMB_SPEED * delta * 2 * climbing)
+
+# Handles the wall sliding of character.
+func wall_climb_func(delta: float, on_floor: bool) -> void:
+	# When walking against wall, start climbing
+	if on_floor:
+		climb_dir = -1
+		velocity.y -= float(WALL_CLIMB_SPEED) / 3
+	
+	# -1 is upwards, 1 is downwards
+	if Input.is_action_pressed("move_down") or Input.is_action_pressed("move_up"):
+		climb_dir = int(Input.get_axis("move_up", "move_down"))
+
+	var move_to: float = WALL_CLIMB_SPEED * climb_dir
+#
+	if abs(velocity.y) > abs(WALL_CLIMB_SPEED):
+		velocity.y = move_toward(velocity.y, move_to, WALL_CLIMB_SPEED * delta * 18)
+	else:
+		velocity.y = move_toward(velocity.y, move_to, WALL_CLIMB_SPEED * delta * 6)
+
+
 
 # Handles all the dash abilities
-func dash_func	(dash: bool, direction: int, on_floor: int) -> void:
+func dash_func	(direction: int, on_floor: int) -> void:
 	if on_floor:
 		dash = true
 	# Checks if dashing is not activated
@@ -81,8 +93,7 @@ func dash_func	(dash: bool, direction: int, on_floor: int) -> void:
 
 func _physics_process(delta):
 	var on_floor = is_on_floor()
-	var on_wall = is_on_wall_only()
-	var dash: bool = true;
+	var on_wall = is_on_wall()
 
 	# Handles gravity
 	gravity_func(delta, on_floor, on_wall)
@@ -96,15 +107,21 @@ func _physics_process(delta):
 	# Handle the horizontal movement/deceleration.
 	move_horizontal(direction)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
+	#Handle jump.
+	if Input.is_action_pressed("jump"):
 		jump_func(Coyote_Jump.get_can_jump())
 	
-	wall_slide_func(delta, on_floor, on_wall)
+	if !on_wall:
+		if velocity.y > 0:
+			climb_dir = 1
+		else:
+			climb_dir = -1
+	if on_wall:
+		wall_climb_func(delta, on_floor)
 
 	# Handles dashing
-	if Input.is_action_just_pressed("dash"):
-		dash_func(dash, direction, on_floor)
+	if Input.is_action_pressed("dash"):
+		dash_func(direction, on_floor)
 	
 	move_and_slide()
 
