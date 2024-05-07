445bffce40cfd3a8b8c96cc4f46cf68291529615
David Angulo Velez <David Angulo Velez>
2024-05-07 23:44:30 +0200

    Starting work on beehive

--- game/Characters/Entities/Flower/Pollenised.gd
+++ game/Characters/Entities/Flower/Pollenised.gd
@@ -2,16 +2,20 @@ extends State
 
 signal Pollen_Add
 
+var current_state := false
+
 func enter():
 	# Iets met uiterlijk, komt later
+	current_state = true
 	pass
 
 func exit():
 # Iets met uiterlijk, komt later
+	current_state = false
 	pass
 
 func _on_hitbox_body_entered(body : Player):
-	if body is Player:
+	if body is Player and current_state == true:
 		body.inventory.add_pollen(1)
 		Pollen_Add.emit(body.name)
 		Transition.emit(self, "Depollenised")
